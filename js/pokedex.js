8d6a49bf76f3dd4a029758feda8ffaa71f28d5aa
Melaine Boue <melaineboue@gmail.com>
2018-01-30 23:31:52 +0100

    Q5

--- js/pokedex.js
+++ js/pokedex.js
@@ -3,6 +3,20 @@ var pokeApp = angular.module('pokedex', ['ngResource']);
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
+pokeApp.controller('monController',function ($scope) {
+
+    $scope.pokemons = [
+        {name:"Scald",id:54},
+        {name:"Round",id:55},
+        {name:"Soak",id:56},
+        {name:"Synchronoise",id:57},
+        {name:"Telekinesis",id:58},
+        {name:"Psyshock",id:59},
+        {name:"Wonder rom",id:60}
+    ];
+    
+});
+
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
