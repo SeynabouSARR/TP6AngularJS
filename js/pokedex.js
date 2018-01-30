ac55af0b69f032fc868c5bd0ba2591cd6bb312fb
Seynabou SARR <seynabousarr90@yahoo.fr>
2018-01-30 23:35:36 +0100

    modif

--- js/pokedex.js
+++ js/pokedex.js
@@ -2,6 +2,17 @@ var pokeApp = angular.module('pokedex', ['ngResource']);
 
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+pokeApp.controller('controller',function ($scope) {
+    $scope.list=[
+        "Scald",
+        "Round",
+        "Soak",
+        "Synchronoise",
+        "Telekinesis",
+        "Psyshock",
+        "Wonder-room"
+    ]
+});
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
