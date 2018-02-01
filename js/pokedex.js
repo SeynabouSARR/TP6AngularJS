135fd6d60066328c472b43ee4a9b59e77ad2a7f9
Seynabou SARR <seynabousarr90@yahoo.fr>
2018-02-01 21:17:35 +0100

    Q11 : Créer un service qui utilise $resource pour accéder aux informations d'un pokémon.

--- js/pokedex.js
+++ js/pokedex.js
@@ -3,54 +3,79 @@ var pokeApp = angular.module('pokedex', ['ngResource']);
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
-pokeApp.controller('monController',function ($scope,$log,$http) {
+pokeApp.controller('monController',['$scope','$log','$http','pokeS',function ($scope,$log,$http,pokeS) {
 
     $scope.pokemons = [
-        {name:"Scald",id:'54'},
-        {name:"Round",id:'55'},
-        {name:"Soak",id:'56'},
-        {name:"Synchronoise",id:'57'},
-        {name:"Telekinesis",id:'58'},
-        {name:"Psyshock",id:'59'},
-        {name:"Wonder rom",id:'60'}
+        {name: "Scald", id: '54'},
+        {name: "Round", id: '55'},
+        {name: "Soak", id: '56'},
+        {name: "Synchronoise", id: '57'},
+        {name: "Telekinesis", id: '58'},
+        {name: "Psyshock", id: '59'},
+        {name: "Wonder rom", id: '60'}
     ];
 
-    $scope.$watch('id',function(){
+    $scope.$watch('id', function () {
         var trouve = false;
         $scope.pokemons.forEach(function (pokemon) {
-            if(pokemon.id == $scope.id)
+            if (pokemon.id == $scope.id)
             {
-                $scope.choix = pokemon.name ;
+                $scope.choix = pokemon.name;
                 trouve = true;
             }
 
 
         })
 
-        });
-    $scope.log=function () {
-       $http.get("https://pokeapi.co/api/v1/pokedex/").then(function (response) {
-           console.log(response.data)
-           $scope.content=response.data;
-       })  }
+    });
+    $scope.log = function () {
+        $log.log($scope.choix);
+    };
 
+    $scope.log = function () {
+        console.log(pokeS);
+        pokeS.listAll().$promise.then(function (data) {
+            console.log(data);
+            console.log(data);
+            $scope.content = data.results;
+        });
+    }
+
+        /* $http.get("https://pokeapi.co/api/v1/pokedex/").then(function (response) {
+             console.log(response.data.objects[0].pokemon)
+             $scope.content=response.data;
+         }) */
+    }]);
+
+pokeApp.factory('pokeS',function($resource){
+    return $resource('https://pokeapi.co/api/v2/pokemon/:id', {id:"@id"},
+        {
+            listAll: { url: 'https://pokeapi.co/api/v2/pokemon/',
+                method: 'GET', params: {}}
+            /* 'get':    {method:'GET'},
+             'save':   {method:'POST'},
+             'query':  {method:'GET', isArray:true},
+             'remove': {method:'DELETE'},
+             'delete': {method:'DELETE'}*/
+        }
+        );
 
 });
-pokeApp.config(['$resourceProvider', function($resourceProvider) {
-    $resourceProvider.defaults.stripTrailingSlashes = false;
- /*   var Pokemon = $resource("http://pokeapi.co/api/v1/type/:id/",
-        {userId:123, nameId:'@id'}, {
-            charge: {method:'POST', params:{charge:true}}
-        });
-    var pokemons = Pokemon.query(function() {
-        var pokemon = pokemons[0];
-        expect(pokemon instanceof Pokemon).toEqual(true);
-        pokemon.name = "J";
+    /*pokeApp.config(['$resourceProvider', function($resourceProvider) {
+       $resourceProvider.defaults.stripTrailingSlashes = false;
+      var Pokemon = $resource("http://pokeapi.co/api/v1/type/:id/",
+           {userId:123, nameId:'@id'}, {
+               charge: {method:'POST', params:{charge:true}}
+           });
+       var pokemons = Pokemon.query(function() {
+           var pokemon = pokemons[0];
+           expect(pokemon instanceof Pokemon).toEqual(true);
+           pokemon.name = "J";
 
-        pokemon.$save();
+           pokemon.$save();
 
-        pokemon.$charge({amount:9.99});
+           pokemon.$charge({amount:9.99});
 
-    });*/
+       });*/
 
-}]);
+
